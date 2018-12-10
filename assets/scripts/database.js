bd95ad45fcfe3b89558e3482eeff7f2795a5241b
unknown <melky025@mail.ru>
2018-12-10 20:57:32 +0300

    progres

--- assets/scripts/database.js
+++ assets/scripts/database.js
@@ -1,67 +1,113 @@
+$(document).ready(function () { 
+
+
 
 let figuries = [
 	{
-		name: 
-		cost
-		size
+		img:'assets/imgs/anime.jpg',
+		name:'Eva',
+		size:'100X500Cm',
+		cost: 700,
+		type:'Жулик несчастный'
+
+	},
+	{
+		img:'assets/imgs/anime.jpg',
+		name:'FT',
+		size:'50X70Cm',
+		cost: 100,
+		type:'Жулик несчастный'
+
+	},
+	{
+		img:'assets/imgs/anime.jpg',
+		name:'Ov',
+		size:'1000X700Cm',
+		cost: 1400,
+		type:'Жулик несчастный'
+
+	},
+	{
+		img:'assets/imgs/anime.jpg',
+		name:'MHA',
+		size:'1000X700Cm',
+		cost: 1400,
+		type:'Жулик несчастный'
 
 	}
-]
-figuries.ex = {
-'<div class="figure">\
-			<img class="test" src="assets/imgs/anime.jpg">\
-			<div class="nice">Аниме:<span>-------</span></div>\
-			<div class="nice">Размер:<span>1000Х700Cm</span></div>\
-			<div class="nice">Описание:<span>Бублики были вкусные, ириски не люблю, Религией не увлекаюсь</span></div>\
-			<div class="nice">Вид:<span>прекрасный</span></div>\
-		</div>\
-				<div class="figure">\
-			<img class="test" src="assets/imgs/anime.jpg">\
-			<div class="nice">Аниме:<span>-------</span></div>\
-			<div class="nice">Размер:<span>1000Х700Cm</span></div>\
-			<div class="nice">Описание:<span>Бублики были вкусные, ириски не люблю, Религией не увлекаюсь</span></div>\
-			<div class="nice">Вид:<span>прекрасный</span></div>\
-		</div>\
-		<div class="figure">\
-			<img class="test" src="assets/imgs/anime.jpg">\
-			<div class="nice">Аниме:<span>-------</span></div>\
-			<div class="nice">Размер:<span>1000Х700Cm</span></div>\
-			<div class="nice">Описание:<span>Бублики были вкусные, ириски не люблю, Религией не увлекаюсь</span></div>\
-			<div class="nice">Вид:<span>прекрасный</span></div>\
-		</div>\
-				<div class="figure">\
-			<img class="test" src="assets/imgs/anime.jpg">\
-			<div class="nice">Аниме:<span>-------</span></div>\
-			<div class="nice">Размер:<span>1000Х700Cm</span></div>\
-			<div class="nice">Описание:<span>Бублики были вкусные, ириски не люблю, Религией не увлекаюсь</span></div>\
-			<div class="nice">Вид:<span>прекрасный</span></div>\
-		</div>\
-<div class="figure">\
-			<img class="test" src="assets/imgs/anime.jpg">\
-			<div class="nice">Аниме:<span>-------</span></div>\
-			<div class="nice">Размер:<span>1000Х700Cm</span></div>\
-			<div class="nice">Описание:<span>Бублики были вкусные, ириски не люблю, Религией не увлекаюсь</span></div>\
-			<div class="nice">Вид:<span>прекрасный</span></div>\
-		</div>\
-				<div class="figure">\
-			<img class="test" src="assets/imgs/anime.jpg">\
-			<div class="nice">Аниме:<span>-------</span></div>\
-			<div class="nice">Размер:<span>1000Х700Cm</span></div>\
-			<div class="nice">Описание:<span>Бублики были вкусные, ириски не люблю, Религией не увлекаюсь</span></div>\
-			<div class="nice">Вид:<span>прекрасный</span></div>\
-		</div>\
-<div class="figure">\
-			<img class="test" src="assets/imgs/anime.jpg">\
-			<div class="nice">Аниме:<span>-------</span></div>\
-			<div class="nice">Размер:<span>1000Х700Cm</span></div>\
-			<div class="nice">Описание:<span>Бублики были вкусные, ириски не люблю, Религией не увлекаюсь</span></div>\
-			<div class="nice">Вид:<span>прекрасный</span></div>\
-		</div>\
-				<div class="figure">\
-			<img class="test" src="assets/imgs/anime.jpg">\
-			<div class="nice">Аниме:<span>-------</span></div>\
-			<div class="nice">Размер:<span>1000Х700Cm</span></div>\
-			<div class="nice">Описание:<span>Бублики были вкусные, ириски не люблю, Религией не увлекаюсь</span></div>\
-			<div class="nice">Вид:<span>прекрасный</span></div>\
-		</div>'		
-}
+
+];	
+
+let result = '';
+figuries.forEach((goods) => {
+	 result += '<div class="figure">\
+ 			<img class="test" src="' + goods.img + '">\
+ 			<div class="nice">Аниме:<span>' + goods.name + '</span></div>\
+ 			<div class="nice">Размер:<span>' + goods.size + '</span></div>\
+ 			<div class="nice">Цена:<span>' + goods.cost + '</span></div>\
+ 			<div class="nice">Вид:<span>' + goods.type + '</span></div>\
+ 		</div>';		
+});
+
+$('.shoplist').append(result);
+
+
+});
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// figuries.ex = {
+// '<div class="figure">\
+// 			<img class="test" src="assets/imgs/anime.jpg">\
+// 			<div class="nice">Аниме:<span>-------</span></div>\
+// 			<div class="nice">Размер:<span>1000Х700Cm</span></div>\
+// 			<div class="nice">Цена:<span>1400</span></div>\
+// 			<div class="nice">Описание:<span>Бублики были вкусные, ириски не люблю, Религией не увлекаюсь</span></div>\
+// 			<div class="nice">Вид:<span>прекрасный</span></div>\
+// 		</div>'		
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,12 +3,7 @@ $(document).ready(function () {
 	$('.js-example-basic-multiple').select2();
 
 
-	let  shop = {}
-	shop.ex = {
-		                             
-	}
-
-
+	
 
 
 
