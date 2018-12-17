708a56810c1288d4ff601ddc2f997eda81ec2c7c
unknown <melky025@mail.ru>
2018-12-17 20:55:59 +0300

    hr

--- assets/scripts/database.js
+++ assets/scripts/database.js
@@ -1,58 +1,263 @@
-$(document).ready(function () { 
-
-
-
 let figuries = [
 	{
 		img:'assets/imgs/anime.jpg',
-		name:'Eva',
+		name:'not from anime',
 		size:'100X500Cm',
 		cost: 700,
-		type:'Жулик несчастный'
+		type:'Dy'
 
 	},
 	{
-		img:'assets/imgs/anime.jpg',
-		name:'FT',
+		img:'assets/imgs/kirisu.jpg',
+		name:'Врата Штейнера',
 		size:'50X70Cm',
 		cost: 100,
-		type:'Жулик несчастный'
+		type:'Da'
 
 	},
 	{
-		img:'assets/imgs/anime.jpg',
-		name:'Ov',
+		img:'assets/imgs/angel.jpg',
+		name:'not from anime',
 		size:'1000X700Cm',
 		cost: 1400,
-		type:'Жулик несчастный'
+		type:'Dy'
 
 	},
 	{
-		img:'assets/imgs/anime.jpg',
-		name:'MHA',
+		img:'assets/imgs/aos.jpg',
+		name:'Стрелок с Черной горы',
+		size:'1000X700Cm',
+		cost: 1000,
+		type:'MA'
+
+	},
+	{
+		img:'assets/imgs/asuna.jpg',
+		name:'Мастера Меча Онлайн',
 		size:'1000X700Cm',
 		cost: 1400,
-		type:'Жулик несчастный'
+		type:'Da'
+
+	},
+	{
+		img:'assets/imgs/blackr.jpeg',
+		name:'Стрелок с Черной горы',
+		size:'100X70Cm',
+		cost: 100,
+		type:'MA'
+
+	},
+	{
+		img:'assets/imgs/blueex.jpg',
+		name:'Синий Экзорцист',
+		size:'1200X700Cm',
+		cost: 1200,
+		type:'Da'
+
+	},
+	{
+		img:'assets/imgs/codegeas.jpg',
+		name:'Код Гиас',
+		size:'1100X700Cm',
+		cost: 1200,
+		type:'Dy'
+
+	},
+	{
+		img:'assets/imgs/ais.jpeg',
+		name:'В подземелье я пойду там красавицу найду',
+		size:'1000X700Cm',
+		cost: 1000,
+		type:'Da'
+
+	},
+	{
+		img:'assets/imgs/ais.jpg',
+		name:'В подземелье я пойду там красавицу найду',
+		size:'1000X700Cm',
+		cost: 1400,
+		type:'MA'
+
+	},
+	{
+		img:'assets/imgs/gatesteins.jpg',
+		name:'Врата Штейнера',
+		size:'1200X700Cm',
+		cost: 1200,
+		type:'Dy'
+
+	},
+	{
+		img:'assets/imgs/gg.jpg',
+		name:'Код Гиас',
+		size:'100X70Cm',
+		cost: 100,
+		type:'MA'
+
+	},
+	{
+		img:'assets/imgs/gundam.jpg',
+		name:'Гандам',
+		size:'1000X700Cm',
+		cost: 1400,
+		type:'Da'
+
+	},
+	{
+		img:'assets/imgs/hestia.jpg',
+		name:'В подземелье я пойду там красавицу найду',
+		size:'100X70Cm',
+		cost: 700,
+		type:'MA'
+
+	},
+	{
+		img:'assets/imgs/hinata.jpg',
+		name:'Волейбол',
+		size:'100X70Cm',
+		cost: 100,
+		type:'Dy'
+
+	},
+	{
+		img:'assets/imgs/isuzu.jpg',
+		name:'Великолепный парк Амаги',
+		size:'1000X700Cm',
+		cost: 1400,
+		type:'MA'
+
+	},
+	{
+		img:'assets/imgs/jjjj.jpg',
+		name:'В подземелье я пойду там красавицу найду',
+		size:'100X70Cm',
+		cost: 100,
+		type:'Dy'
+
+	},
+	{
+		img:'assets/imgs/kirito.jpg',
+		name:'Мастера Меча Онлайн',
+		size:'1000X700Cm',
+		cost: 1400,
+		type:'Da'
+
+	},
+	{
+		img:'assets/imgs/kiritogirl.jpg',
+		name:'Мастера Меча Онлайн',
+		size:'100X70Cm',
+		cost: 100,
+		type:'MA'
+
+	},
+	{
+		img:'assets/imgs/mefisto.jpg',
+		name:'Синий Экзорцист',
+		size:'1200X700Cm',
+		cost: 1200,
+		type:'Da'
+
+	},
+	{
+		img:'assets/imgs/mesh.jpeg',
+		name:'not from anime',
+		size:'100X700Cm',
+		cost: 100,
+		type:'Dy'
+
+	},
+	{
+		img:'assets/imgs/neito.jpg',
+		name:'not from anime',
+		size:'100X70Cm',
+		cost: 100,
+		type:'MA'
+
+	},
+	{
+		img:'assets/imgs/nendroidkirisu.jpg',
+		name:'Врата Штейнера',
+		size:'1100X700Cm',
+		cost: 1200,
+		type:'Da'
+
+	},
+	{
+		img:'assets/imgs/neonoid.jpg',
+		name:'not from anime',
+		size:'1400X700Cm',
+		cost: 1400,
+		type:'Dy'
+
+	},
+	{
+		img:'assets/imgs/nishinoya.jpg',
+		name:'Волейбол',
+		size:'1000X700Cm',
+		cost: 1400,
+		type:'MA'
+
+	},
+	{
+		img:'assets/imgs/neiza.jpeg',
+		name:'not from anime',
+		size:'1000X700Cm',
+		cost: 1400,
+		type:'MA'
+
+	},
+	{
+		img:'assets/imgs/parkamagi.jpg',
+		name:'Великолепный парк Амаги',
+		size:'100X70Cm',
+		cost: 100,
+		type:'Da'
+
+	},
+	{
+		img:'assets/imgs/rin.jpg',
+		name:'Синий Экзорцист',
+		size:'100X700Cm',
+		cost: 1000,
+		type:'Dy'
+
+	},
+	{
+		img:'assets/imgs/sinon.jpeg',
+		name:'Мастера Меча Онлайн',
+		size:'1000X700Cm',
+		cost: 1000,
+		type:'Da'
+
+	},
+	{
+		img:'assets/imgs/tobio.jpg',
+		name:'Волейбол',
+		size:'100X70Cm',
+		cost: 100,
+		type:'MA'
+
+	},
+	{
+		img:'assets/imgs/yukio.jpg',
+		name:'Синий Экзорцист',
+		size:'1000X700Cm',
+		cost: 1000,
+		type:'Dy'
+
+	},
+	{
+		img:'assets/imgs/whoo.jpg',
+		name:'not from anime',
+		size:'100X70Cm',
+		cost: 100,
+		type:'Da'
 
 	}
+	
 
-];	
-
-let result = '';
-figuries.forEach((goods) => {
-	 result += '<div class="figure">\
- 			<img class="test" src="' + goods.img + '">\
- 			<div class="nice">Аниме:<span>' + goods.name + '</span></div>\
- 			<div class="nice">Размер:<span>' + goods.size + '</span></div>\
- 			<div class="nice">Цена:<span>' + goods.cost + '</span></div>\
- 			<div class="nice">Вид:<span>' + goods.type + '</span></div>\
- 		</div>';		
-});
-
-$('.shoplist').append(result);
-
-
-});
+];	  
 	
 
 
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,8 +3,51 @@ $(document).ready(function () {
 	$('.js-example-basic-multiple').select2();
 
 
-	
 
+	$('.js-example-basic-multiple').change(function(event){
+		console.log(event.target.classList[0]);	
+		let filter = [];
+		switch(event.target.classList[0]){
+			case 'size':
+			filter = figuries.filter(function(current){
+				return current.size === event.target.options[event.target.selectedIndex].value;
+			});console.log(filter);
+			break;
+			case 'anime':
+			break;
+			case 'cost':
+			break;
+			case 'type':
+			break;
+		};
+		let result = '';
+		filter.forEach((goods) => {
+			 result += '<div class="figure">\
+		 			<img class="test" src="' + goods.img + '">\
+		 			<div class="nice">Аниме:<span>' + goods.name + '</span></div>\
+		 			<div class="nice">Размер:<span>' + goods.size + '</span></div>\
+		 			<div class="nice">Цена:<span>' + goods.cost + '</span></div>\
+		 			<div class="nice">Вид:<span>' + goods.type + '</span></div>\
+		 		</div>';		
+		});
+		$('.shoplist').empty();
+		$('.shoplist').append(result);
+	});
+
+
+	
+let result = '';
+figuries.forEach((goods) => {
+	 result += '<div class="figure">\
+ 			<img class="test" src="' + goods.img + '">\
+ 			<div class="nice">Аниме:<span>' + goods.name + '</span></div>\
+ 			<div class="nice">Размер:<span>' + goods.size + '</span></div>\
+ 			<div class="nice">Цена:<span>' + goods.cost + '</span></div>\
+ 			<div class="nice">Вид:<span>' + goods.type + '</span></div>\
+ 		</div>';		
+});
+
+$('.shoplist').append(result);
 
 
 
