7ec91372425b606a5832ccd40fd930d8f62d0620
unknown <melky025@mail.ru>
2018-12-24 21:01:45 +0300

    fefr

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,44 +2,47 @@ $(document).ready(function () {
 
 	$('.js-example-basic-multiple').select2();
 
+let currentarray = figuries.map(function(item){ return item; });
 
+	
 
 	$('.js-example-basic-multiple').change(function(event){
-		let filter = [];
-		switch(event.target.classList[0]){
-			case 'size':
-				filter = figuries.filter(function(current){
-					return event.target.options[event.target.selectedIndex] === undefined ? true
-					 : current.size === event.target.options[event.target.selectedIndex].value;
+
+			console.log($(".size option:selected").text())
+
+
+				currentarray = figuries.filter(function(current){
+					return $(".size option:selected").text() === '' ? true
+					 : current.size === $(".size option:selected").text() + 'Cm';
 				});
-				break;
-			case 'anime':
-				filter = figuries.filter(function(current){
-					return event.target.options[event.target.selectedIndex] === undefined ? true
-					 : current.name === event.target.options[event.target.selectedIndex].value;
+				console.log(currentarray)
+				currentarray = currentarray.filter(function(current){
+					return $(".anime option:selected").text() === '' ? true
+					 : current.name === $(".anime option:selected").text();
 				});
-				break;
-			case 'cost':
-				filter = figuries.filter(function(current){
-					return event.target.options[event.target.selectedIndex] === undefined ? true
-					 : current.cost == event.target.options[event.target.selectedIndex].value;
+				console.log(currentarray)
+				currentarray = currentarray.filter(function(current){
+						return $(".cost option:selected").text() === '' ? true
+					 : current.cost == $(".cost option:selected").text();				
 				});
-				break;
-			case 'type':
-				filter = figuries.filter(function(current){
-					return event.target.options[event.target.selectedIndex] === undefined ? true
-					 : current.type === event.target.options[event.target.selectedIndex].value;
+				console.log(currentarray)
+				currentarray = currentarray.filter(function(current){
+					return $(".type option:selected").text() === '' ? true
+					 : current.type === $(".type option:selected").text();
 				});
-			break;
-		};
+				console.log(currentarray)
+	
+
 		let result = '';
-		filter.forEach((goods) => {
+		currentarray.forEach((goods) => {
 			 result += '<div class="figure">\
 		 			<img class="test" src="' + goods.img + '">\
 		 			<div class="nice">Аниме:<span>' + goods.name + '</span></div>\
 		 			<div class="nice">Размер:<span>' + goods.size + '</span></div>\
 		 			<div class="nice">Цена:<span>' + goods.cost + '</span></div>\
 		 			<div class="nice">Вид:<span>' + goods.type + '</span></div>\
+		 			<button class="podrobnee">Подробнее</button>\
+					<button class="buy">Купить</button>\
 		 		</div>';		
 		});
 		$('.shoplist').empty();
@@ -64,5 +67,12 @@ figuries.forEach((goods) => {
 $('.shoplist').append(result);
 
 
+
+ $('.figure').on('click', '.podrobnee', function(e){
+ 	let card = $(e.target).parent();
+ 	console.log();
+ 	card.append('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut quisquam repudiandae, tenetur quos ullam. Suscipit nobis nostrum qui sit harum, ad quos quis provident, maiores, porro labore excepturi laborum?');
+ 	card.style..height(40);
+ });
 
 });
